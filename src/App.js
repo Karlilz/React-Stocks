24835a5609dc8cf82890e36ffa66be45c77824cb
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-15 18:14:53 -0400

    Readme File created, components and pages folder created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Route, Routes } from "react-router-dom";
+import Nav from './components/Nav';
+import About from './pages/About';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Stock from './pages/Stock';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav/>
+    <h1>Stock Savvy</h1>
+    <About/>
+    <Dashboard/>
+    <Home/>
+    <Stock/>
     </div>
   );
 }
